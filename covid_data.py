996161374285df6f711dfc00d45a9b96e4604f4b
Christina Lin <44586776+clin1967@users.noreply.github.com>
2021-12-12 11:50:24 -0500

    Update covid_data.py

--- covid_data.py
+++ covid_data.py
@@ -5,30 +5,27 @@ import pandas
 
 
 def get_covid_data() -> pandas.DataFrame:
-    """
-    Retrieves owid-covid-data.csv and returns it as a DataFrame.
+    """Retrieves owid-covid-data.csv and returns it as a DataFrame.
     """
     data = pandas.read_csv('https://covid.ourworldindata.org/data/owid-covid-data.csv')
     return data
 
 
 def pretty(header: str) -> dict[str: str]:
-    """
-    Returns a header in a nicer format.
+    """Returns a header in a nicer format.
     """
     return ' '.join(header.split('_')).title()
 
 
-def date_to_row(data: pandas.DataFrame, date_set: set[str], csv: bool, overwrite: bool) -> dict:
-    """
-    Returns a dictionary mapping desired dates to each numerical column's value.
+def date_to_row(data: pandas.DataFrame, processed_data: pandas.DataFrame, csv: bool, overwrite: bool) -> dict:
+    """Returns a dictionary mapping desired dates to each numerical column's value.
     if save_to_csv, saves the processed data.
     """
     h_type = dict(data.dtypes)
     dates_dict = {}
 
     for _, row in data.iterrows():
-        if overwrite or row['date'] not in date_set:
+        if overwrite or row['date'] not in processed_data.columns:
 
             if row['date'] not in dates_dict:
                 dates_dict[row['date']] = {}
@@ -54,21 +51,20 @@ def date_to_row(data: pandas.DataFrame, date_set: set[str], csv: bool, overwrite
 
 
 def process_data() -> pandas.DataFrame:
-    """
-    Returns processed_covid_data as a DataFrame.
+    """Returns processed_covid_data as a DataFrame.
     """
     return pandas.read_csv('processed_covid_data.csv')
 
 
 def overwrite_processed_covid_data(data: pandas.DataFrame) -> dict:
-    """Fetches the newest version of the OWID Covid Data and overwrites the spreadsheet
-        with newly processed values.
+    """ Fetches the newest version of the OWID Covid Data and overwrites the spreadsheet
+        with newly processed values. Returns in dict format.
     """
-    return date_to_row(data, set(), csv=True, overwrite=True)
+    return date_to_row(data, pandas.DataFrame(), csv=True, overwrite=True)
 
 
 def update_processed_covid_data(data: pandas.DataFrame, processed_data: pandas.DataFrame) -> dict:
     """Fetches the newest version of the OWID Covid Data and updates the processed spreadsheet
-    with missing dates' values.
+    with missing dates' values. Returns in dict format.
     """
-    return date_to_row(data, set(processed_data.columns), csv=True, overwrite=False)
+    return date_to_row(data, processed_data, csv=True, overwrite=False)
