9385a153e267760149009089570e87c5b8525c8a
Christina Lin <44586776+clin1967@users.noreply.github.com>
2021-12-13 11:35:34 -0500

    Fixed Twint Token Expiry Issue

--- tweet_data.py
+++ tweet_data.py
@@ -3,32 +3,39 @@ Code for retrieving and processing Tweets scraped from Twitter.
 """
 from datetime import timedelta
 from datetime import datetime
-from os import remove
-from os import path
+from ast import literal_eval
+from twint import token
+import backoff
 import pandas
 import twint
-import time
 
 
-def datetime_to_twint(date: datetime) -> str:
+def to_twint(date: datetime) -> str:
     """Returns a datetime in Twitter format.
 
-    >>> datetime_to_twint(datetime(2021, 1, 2, 23, 59))
+    >>> to_twint(datetime(2021, 1, 2, 23, 59))
     '2021-01-02'
     """
     return date.strftime('%Y-%m-%d')
 
 
-def twint_to_datetime(date: str) -> datetime: # CALLED TO FIND END_DATE.
+def to_datetime(date: str) -> datetime: # CALLED TO FIND END_DATE.
     """Returns a date in Twitter format as datetime.
 
-    >>> twint_to_datetime('2021-01-02')
+    >>> to_datetime('2021-01-02')
     datetime.datetime(2021, 1, 2, 23, 59)
     """
-    return datetime.strptime(date + ' 23:59:00', '%Y-%m-%d %H:%M:%S')
+    if '23:59:00' not in date:
+        date += ' 23:59:00'
+    return datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
 
 
-def retrieve_day_tweets(date: datetime, store: bool) -> tuple[datetime, list]:
+@backoff.on_exception(
+    backoff.expo,
+    twint.token.RefreshTokenException,
+    max_tries=20
+)
+def retrieve_day_tweets(date: datetime) -> tuple[datetime, list]:
     """Return a tuple with 20 COVID-related tweets from a given day, formatted as (date, tweets).
     if store is True, saves the raw data to a csv.
     """
@@ -37,12 +44,7 @@ def retrieve_day_tweets(date: datetime, store: bool) -> tuple[datetime, list]:
     twt = twint.Config()
     twt.Lang = 'en'
     twt.Limit = 20
-    twt.Search = 'covid until:' + datetime_to_twint(date + timedelta(days=1)) + ' since:' + datetime_to_twint(date)
-
-    # Store to Raw Data CSV if overwrite is True
-    if store:
-        twt.Store_csv = True
-        twt.Output = 'raw_twitter_data_sample.csv'
+    twt.Search = 'covid until:' + to_twint(date + timedelta(days=1)) + ' since:' + to_twint(date)
 
     # Raw Data List
     raw_tweets = []
@@ -57,44 +59,40 @@ def retrieve_day_tweets(date: datetime, store: bool) -> tuple[datetime, list]:
     return (date, tweets)
 
 
-def retrieve_new_tweets(n: int, end_date: datetime, overwrite: bool) -> tuple[datetime, list[tuple]]:
-    """Returns n days' worth of tweets. Saves the results to a csv.
+@backoff.on_exception(
+    backoff.expo,
+    twint.token.RefreshTokenException,
+    max_tries=10
+)
+def retrieve_new_tweets(n: int, end_date: datetime, save: bool) -> tuple[datetime, list[tuple[datetime, list]]]:
+    """Returns n days' worth of tweets. Saves the results to a csv when save is True.
     Tuple maps the end of the week to (date, list of tweets).
     """
     current_date = end_date
     week_so_far = []
 
-    if overwrite:
-        file = 'raw_twitter_data_sample.csv'
-        if path.exists(file):
-            remove(file)
-
     for _ in range(n):
         day_tweets = (current_date, [])
         while day_tweets[1] == []:
-            day_tweets = retrieve_day_tweets(current_date, overwrite)
-            time.sleep(10)
+            day_tweets = retrieve_day_tweets(current_date)
         week_so_far.append(day_tweets)
         current_date -= timedelta(days=1)
+
+    if save:
+        save_to_raw_data(week_so_far)
 
     return (end_date, week_so_far)
 
 
-# WARNING: Because of Twitter limits, TWINT is not always able to fetch new tweets without token expiry.
-# May need to wait, or use previously-fetched data (below).
-
-
-def pull_from_raw_data() -> tuple[datetime, list[tuple]]:
-    """ Returns the data in raw_twitter_data_sample.csv.
+def save_to_raw_data(data: list[tuple[datetime, list]]) -> None:
+    """ Saves retrieved tweets to twitter_data.csv.
     """
-    data = pandas.read_csv('raw_twitter_data_sample.csv')
-    temp_dict = {}
+    # noinspection PyTypeChecker
+    pandas.DataFrame(data, columns=['date', 'tweets']).to_csv('twitter_data.csv', index=False)
 
-    for _, row in data.iterrows():
-        date = twint_to_datetime(row[3])
-        if date not in temp_dict:
-            temp_dict[date] = []
-        temp_dict[date].append(row[10])
 
-    lst = [(key, temp_dict[key]) for key in temp_dict]
-    return (lst[0][0], lst)
+def pull_from_raw_data() -> list[tuple[datetime, list]]:
+    """ Returns the data in twitter_data.csv.
+    """
+    data = pandas.read_csv('twitter_data.csv').values.tolist()
+    return [(to_datetime(pair[0]), literal_eval(pair[1])) for pair in data]
